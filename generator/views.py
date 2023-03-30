3a5093cfa52daf6178f0cf8ab998fe82901dcc42
pogadaevi <pogadaevi@bzz.ch>
2023-03-30 23:08:33 +0200

    small changes

--- generator/views.py
+++ generator/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import *
-
+import random
 
 # Create your views here.
 
@@ -9,4 +9,16 @@ def home(request):
 
 
 def password(request):
-    return render(request, 'generator/password.html')
+
+    characters=list('abcdefghijklmnopqrstuwvyz')
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHHIJKLMNOPQRSTUWVYZ'))
+    if request.GET.get('numbers'):
+        characters.extend(list('ABCDEFGHHIJKLMNOPQRSTUWVYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('ABCDEFGHHIJKLMNOPQRSTUWVYZ'))
+    length=int(request.GET.get('length'))
+    thepassword=''
+    for x in range(length):
+        thepassword+=random.choice(characters)
+    return render(request, 'generator/password.html', {'password':thepassword})
